bd5de933fed6459db5ae51124c003e0b440a5844
georgy.m <iam@georgy.top>
2024-01-04 12:28:42 +0300

    Fix readme

--- semopy/__init__.py
+++ semopy/__init__.py
@@ -1,7 +1,7 @@
 """semopy: Structural Equation Modeling Optimization in Python"""
 
 name = "semopy"
-__version__ = "2.3.10"
+__version__ = "2.3.11"
 __author__ = "Georgy Meshcheryakov"
 
 from .model_generalized_effects import ModelGeneralizedEffects
